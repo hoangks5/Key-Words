e1d8a5cd88045b93ea31fd5e56497177dc2db2db
hoangks5 <hoangnd.ute@gmail.com>
2022-09-07 15:20:37 +0700

    --- Commit ---

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import yake
 with open('text.txt','r',encoding='utf-8') as file:
     doc = file.read()
 kw = yake.KeywordExtractor(top = 10, stopwords=None)
-
+keys = kw.extract_keywords(doc)
 
 print(doc)
